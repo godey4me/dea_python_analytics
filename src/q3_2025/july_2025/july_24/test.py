7892fdd5545260b7d7175248576e33c296b89420
TheChanRProject <garpchoo@gmail.com>
2025-07-24 21:57:48 -0700

    Cleaned up test.py

--- src/q3_2025/july_2025/july_24/test.py
+++ src/q3_2025/july_2025/july_24/test.py
@@ -2,13 +2,6 @@ import pandas as pd
 from pandas_util import get_final_normalized_frame, identify_dtype
 from sample_data import json_data
 
-# # Normalize the original DataFrame
-# normalized_df = pd.json_normalize(data=json_data)
-
-# result = identify_dtype(frame=normalized_df, col='orders')
-
-# print(result)
-
 result = get_final_normalized_frame(data=json_data)
 
 print(result)
