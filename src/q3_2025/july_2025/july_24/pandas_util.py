e2488ad4fd05c3aac5b3f1e8fb6baa13135a39ee
TheChanRProject <garpchoo@gmail.com>
2025-07-24 21:57:34 -0700

    Adding updated implementation for pandas_util and test.

--- src/q3_2025/july_2025/july_24/pandas_util.py
+++ src/q3_2025/july_2025/july_24/pandas_util.py
@@ -1,32 +1,94 @@
 import pandas as pd
+from typing import Any
 
-def normalized_frame(data: list[dict]) -> pd.DataFrame:
+# Helper Function - identify_dtype
+def identify_dtype(frame: pd.DataFrame, col: str) -> tuple[Any, type]:
+    """
+    ## Inputs
+    - frame (pandas DataFrame): The original DataFrame
+    - col (string) : The column to apply the data type investigation
 
-    # First DataFrame
+    ## Output
+    - value_dtype (type): The actual data type of the first value of a specific column in the DataFrame
+    """
+
+    # First value that belongs to a column in a DataFrame
+    first_value = frame.iloc[0][col]
+
+    value_dtype = type(first_value)
+
+    return first_value, value_dtype
+
+
+
+# Helper Function
+def clean_explode(frame: pd.DataFrame, col: str) -> pd.DataFrame:
+    """
+    ## Inputs
+    - frame (pandas DataFrame): The original DataFrame with columns that contain list values.
+    - col (string) : Column to explode on.
+
+    ## Output
+    - clean_frame (pandas DataFrame): This DataFrame will explode and also combine the new values into a singular Dataframe.
+    """
+
+    # Explode on the column
+    frame = frame.explode(column=col).reset_index().drop('index', axis=1)
+
+    # The column to explode on as a Series
+    col_as_a_series = frame[col].to_list()
+
+    col_df = pd.json_normalize(data=col_as_a_series).reset_index().drop('index', axis=1)
+
+    # Rename the columns of the column DataFrame
+    col_df.columns = [f'{col}_{internal_col}' for internal_col in col_df.columns.tolist()]
+
+    # Concatenate them
+    clean_frame = pd.concat(objs=[frame, col_df], axis=1)
+
+    return clean_frame
+
+
+
+def get_final_normalized_frame(data: list[dict]) -> pd.DataFrame:
+    """
+    ## Inputs
+    - data (list of dictionaries) : Input data which will become a DataFrame in pandas.
+
+    ## Output
+    - frame (pandas DataFrame)
+    """
+
+    # Originally, normalize the DataFrame
     df = pd.json_normalize(data=data)
 
-    cols_with_list_row = []
+    # Columns that have the list data type
+    cols_with_list_type = []
 
-    # Check if any of my columns have a list type in the first row
     for col in df.columns.tolist():
-        # First row from each column
-        first_value = df[col].iloc[0]
+        value, col_dtype = identify_dtype(frame=df, col=col)
 
-        first_value_type = type(first_value)
+        print(f"Column {col} has dtype: {col_dtype}")
 
-        if isinstance(first_value_type, list):
-            cols_with_list_row.append(col)
+        if isinstance(value, list):
+            # Add the column if the value happens to be a list
+            cols_with_list_type.append(col)
     
-    # Go through the cols with list row
-    if len(cols_with_list_row) > 0:
-        # Explode the columns one by one
-        for col in cols_with_list_row:
-            new_df = df.explode(column=col)
+    # Explode the columns that need it
+    first_col_to_explode = cols_with_list_type[0]
 
-            col_list_dicts = new_df[col].to_list()
+    merged_df = clean_explode(frame=df, col=first_col_to_explode)
 
-            new_normalized_df = pd.json_normalize(data=col_list_dicts)
+    merged_df.drop(first_col_to_explode, axis=1, inplace=True)
 
-            df = pd.concat(objs=[new_df, new_normalized_df], axis=1)
+    # If you have more than one column to explode
+    if len(cols_with_list_type) > 1:
+        # Iterate through the rest of the columns
+        for col in cols_with_list_type[1:]:
+            merged_df = clean_explode(merged_df,col)
+
+            # Drop the exploded column
+            merged_df.drop(col, axis=1, inplace=True)
     
-    return df
+
+    return merged_df
--- src/q3_2025/july_2025/july_24/test.py
+++ src/q3_2025/july_2025/july_24/test.py
@@ -1,52 +1,14 @@
 import pandas as pd
-from pandas_util import normalized_frame
+from pandas_util import get_final_normalized_frame, identify_dtype
 from sample_data import json_data
-from format_util import line_separator
 
-df = normalized_frame(data=json_data)
+# # Normalize the original DataFrame
+# normalized_df = pd.json_normalize(data=json_data)
 
-df.info()
+# result = identify_dtype(frame=normalized_df, col='orders')
 
-print(df.head())
+# print(result)
 
-example_type = type(df['orders'].iloc[0])
-
-print(example_type)
-
-df = df.explode(column='orders')
-
-print(df)
-
-orders_series = df['orders']
-
-print("Series: ", orders_series)
-
-line_separator()
-
-# Individual row
-print("individual row in Series:", orders_series.iloc[0])
-
-line_separator()
-
-# Break it out into list of dictionaries
-list_of_dicts = df['orders'].to_list()
-
-print("List of dictionaries across rows: ", list_of_dicts)
-
-line_separator()
-
-new_df = pd.json_normalize(data=list_of_dicts).reset_index().drop('index', axis=1)
-
-print(df.head())
-
-line_separator()
-
-print(new_df.head())
-
-result = pd.concat(objs=[df.reset_index().drop('index', axis=1), new_df], axis=1)
-
-result.drop('orders', axis=1, inplace=True)
+result = get_final_normalized_frame(data=json_data)
 
 print(result)
-
-
